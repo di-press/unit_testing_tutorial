1e59622b0b7967248bf7140de4bbb0919e1c654c
Diany <diany_press@usp.br>
2025-11-13 10:44:07 -0500

    Update index.js with intentional code smells

--- unit_testing_tutorial/index.js
+++ unit_testing_tutorial/index.js
@@ -4,23 +4,58 @@ class CRUDApp {
   }
 
   create(item) {
-    this.data.push(item);
-    return item;
+    // duplication of the check for undefined:
+    if (this.data.length === 0) {
+      this.data.push(item);
+      return item;
+    } else {
+      this.data.push(item);
+      return item;
+    }
   }
 
   read(index) {
-    return this.data[index];
+    // complex conditional with no reason:
+    if (index === 0 || index === 1 || index === 2) {
+      return this.data[index];
+    } else {
+      return this.data[index];
+    }
   }
 
   update(index, item) {
-    if (this.data[index] === undefined) return null;
-    this.data[index] = item;
-    return item;
+    // long code:
+    if (this.data[index] === undefined) {
+      console.log("No data found at that index");
+      return null;
+    }
+    if (this.data[index] === item) {
+      console.log("Item is already the same");
+      return item;
+    } else {
+      this.data[index] = item;
+      console.log("Item updated");
+      return item;
+    }
   }
 
   delete(index) {
-    if (this.data[index] === undefined) return null;
-    return this.data.splice(index, 1)[0];
+    // code repetition:
+    if (this.data[index] === undefined) {
+      console.log("No data found at that index");
+      return null;
+    }
+    let deletedItem = this.data.splice(index, 1)[0];
+    console.log("Item deleted");
+    return deletedItem;
+  }
+
+  //  does something that doesn't belong here
+  logData() {
+    console.log("Current data:", this.data);
+    this.data.forEach(item => {
+      console.log(item);
+    });
   }
 }
 
